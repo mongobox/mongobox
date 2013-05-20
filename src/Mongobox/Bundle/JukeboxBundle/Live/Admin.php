3a862aabcb4fa60fb16431f384ecd28704660fab
alexandre-jardin <aja@emakina.fr>
2013-05-20 17:34:25 +0200

    Live & Groupes - Initialisation de la playlist depuis un service

--- src/Mongobox/Bundle/JukeboxBundle/Live/Admin.php
+++ src/Mongobox/Bundle/JukeboxBundle/Live/Admin.php
@@ -3,6 +3,8 @@
 namespace Mongobox\Bundle\JukeboxBundle\Live;
 
 use Doctrine\ORM\EntityManager;
+use Mongobox\Bundle\GroupBundle\Entity\Group;
+use Mongobox\Bundle\JukeboxBundle\Entity\Playlist;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class Admin
@@ -45,5 +47,43 @@ class Admin
         }
 
         return ($groupEntity->getLiveCurrentAdmin() === $currentUser);
+    }
+
+    /**
+     * Initialize the jukebox playlist
+     *
+     * @param Group $group
+     * @return Playlist object
+     */
+    public function initializePlaylist(Group $group)
+    {
+        $this->em->getRepository('MongoboxJukeboxBundle:Playlist')->generate($group);
+
+        $currentPlaylist = $this->em->getRepository('MongoboxJukeboxBundle:Playlist')->findOneBy(array(
+            'group'     => $group->getId(),
+            'current'   => 1
+        ));
+
+        if (!is_null($currentPlaylist)) {
+            $votes = $this->em->getRepository('MongoboxJukeboxBundle:Vote')->sommeVotes($currentPlaylist);
+
+            $currentPlaylist->getVideoGroup()->setVotes($currentPlaylist->getVideoGroup()->getVotes() + $votes);
+            $currentPlaylist->getVideoGroup()->setDiffusion($currentPlaylist->getVideoGroup()->getDiffusion() + 1);
+
+            $this->em->getRepository('MongoboxJukeboxBundle:Vote')->wipe($currentPlaylist);
+            $this->em->getRepository('MongoboxJukeboxBundle:Volume')->wipe($currentPlaylist);
+            $this->em->remove($currentPlaylist);
+        }
+
+        $nextInPlaylist = $this->em->getRepository('MongoboxJukeboxBundle:Playlist')->next(1, $group);
+
+        if (is_object($nextInPlaylist)) {
+            $nextInPlaylist->setCurrent(1);
+            $nextInPlaylist->getVideoGroup()->setLastBroadcast(new \Datetime());
+        }
+
+        $this->em->flush();
+
+        return $nextInPlaylist;
     }
 }
