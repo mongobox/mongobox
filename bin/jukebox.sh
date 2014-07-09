11c1d8f0490dbb227d578bf712d49a4097c95eb7
Aurélien GIRY <agi@emakina.fr>
2014-07-09 17:36:29 +0200

    n/a: [jukeboxBundle] - create script to clean videos and integrate Google API Client

---
+++ bin/jukebox.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+## Update and clean videos
+cd /var/www/mongobox
+php app/console jukebox:updateDataYoutube --debug
--- src/Mongobox/Bundle/JukeboxBundle/Command/UpdateYoutubeDataCommand.php
+++ src/Mongobox/Bundle/JukeboxBundle/Command/UpdateYoutubeDataCommand.php
@@ -14,10 +14,24 @@ use Monolog\Processor\MemoryUsageProcessor;
 // Handler
 use Mongobox\Bundle\JukeboxBundle\Command\Monolog\Handler\CliHandler;
 
+// Google API
+use Google_Client;
+use Google_Service_YouTube;
+
+/**
+ * Class UpdateYoutubeDataCommand
+ *
+ * @category    Mongobox
+ * @package     Mongobox\Bundle\JukeboxBundle\Command
+ */
 class UpdateYoutubeDataCommand extends ContainerAwareCommand
 {
+    const YOUTUBE_STATUS_PUBLIC = 'public';
+    const YOUTUBE_STATUS_UNLISTED = 'unlisted';
+
     protected $debug;
-    private $feedUrl;
+
+    private $em;
 
     /**
      * {@inheritDoc}
@@ -25,22 +39,21 @@ class UpdateYoutubeDataCommand extends ContainerAwareCommand
     protected function configure()
     {
         $this
-        ->setName('jukebox:updateDataYoutube')
-        ->setDescription('Udpate data from Youtube.')
-        ->setHelp('')
-        ;
+            ->setName('jukebox:updateDataYoutube')
+            ->setDescription('Udpate data from Youtube.')
+            ->setHelp('');
 
         $this->getDefinition()->addOptions(
-                array(
-                        new InputOption('debug',null,InputOption::VALUE_NONE,'Show Message'),
-                )
+            array(
+                new InputOption('debug', null, InputOption::VALUE_NONE, 'Show Message'),
+            )
         );
     }
 
     /**
      * Call before execute
      *
-     * @param \Symfony\Component\Console\Input\InputInterface   $input
+     * @param \Symfony\Component\Console\Input\InputInterface $input
      * @param \Symfony\Component\Console\Output\OutputInterface $output
      */
     protected function initialize(InputInterface $input, OutputInterface $output)
@@ -57,62 +70,153 @@ class UpdateYoutubeDataCommand extends ContainerAwareCommand
             $this->logger->pushProcessor(new MemoryUsageProcessor());
             $this->logger ->pushHandler(new CliHandler($this->output));
         }
+
+        $this->em = $this->getContainer()->get('doctrine')->getManager('default');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln('<bg=cyan;fg=white>Start traitement </>');
+
+        $output->writeln('<bg=cyan;fg=red>Start traitement </>');
+
+        $client = new Google_Client();
+        $client->setApplicationName("Mongobox");
+        $client->setDeveloperKey("AIzaSyATMXQ_V-Ie8KaJEN0BAzK5bSSK2qvEukQ");
+        $youtube = new Google_Service_YouTube($client);
+
 
         $em = $this->getContainer()->get('doctrine')->getManager('default');
-        $videos = $em->getRepository('MongoboxJukeboxBundle:Videos')->findBy(array('duration' => 0));
+        $videos = $em->getRepository('MongoboxJukeboxBundle:Videos')->findAll();
 
-        $compteurVideo = 0;
+        $compteurVideo = $compteurDelatedVideo = 0;
         foreach ($videos as $key => $video) {
 
             try {
 
-                $this->feedUrl = 'http://gdata.youtube.com/feeds/api/videos/' . $video->getLien();
-                $data = $this->_getFeedData();
+                $response =
+                    $youtube->videos->listVideos("snippet,status,contentDetails", array('id' => $video->getLien()));
 
-                    if ($data) {
+                $items = $response->getItems();
+                if (empty($items)) {
 
-                        $output->writeln('Titre : <fg=green>(id=' . $video->getId() .') ' .  $data->title . '</>');
+                    $output->writeln('Error on video : <fg=red>' . $video->getLien() . '</>');
 
-                        $video
-                            ->setTitle( $data->title )
-                            ->setDuration( $data->duration )
-                            ->setThumbnail( $data->thumbnail->hqDefault )
-                            ->setThumbnailHq( $data->thumbnail->sqDefault )
-                        ;
+                    $this->deleteVideo($video);
 
-                        $em->persist($video);
-                        $em->flush();
+                    $this->output->writeln('Delete video: ' . $video->getLien());
+                    $compteurDelatedVideo++;
 
-                        $compteurVideo++;
-                    } else {
-                        $output->writeln('Error on video : <fg=red>' .  $video->getLien() . '</>');
+                    continue;
+                }
+
+                foreach ($response->getItems() as $youtubeVideo) {
+
+                    $output->writeln('Youtube ID: ' . $youtubeVideo->getId());
+
+                    switch ($youtubeVideo->getStatus()->getPrivacyStatus()) {
+                        case self::YOUTUBE_STATUS_PUBLIC:
+                        case self::YOUTUBE_STATUS_UNLISTED:
+
+                            $snippet = $youtubeVideo->getSnippet();
+
+                            // Duration
+                            $duration = $youtubeVideo->getContentDetails()->getDuration();
+                            $interval = new \DateInterval($duration);
+                            $duration = $this->toSeconds($interval);
+
+                            // Thumbnails
+                            $thumbnails = $snippet->getThumbnails();
+
+                            $video
+                                ->setTitle($snippet->getTitle())
+                                ->setDuration($duration)
+                                ->setThumbnail($thumbnails->getDefault()->getUrl())
+                                ->setThumbnailHq($thumbnails->getHigh()->getUrl());
+                            $em->persist($video);
+                            $em->flush();
+
+                            $output->writeln(
+                                'Update Video: <fg=green>(id=' . $video->getId() . ') ' . $snippet->getTitle() . '</>'
+                            );
+                            $compteurVideo++;
+                            break;
+                        default:
+                            $output->writeln('<bg=red;fg=white>############# New case #############</>');
+                            $output->writeln($youtubeVideo);
+                            break;
                     }
-
+                }
             } catch (Exception $e) {
+                $output->writeln('<bg=red;fg=white>Error: ' . $e->getMessage() . '</>');
                 continue;
             }
-
         }
 
-        $this->logger->addInfo("Nombre de vidéos modifiées : {$compteurVideo}");
+        $this->logger->addInfo("Updated videos: {$compteurVideo}");
+        $this->logger->addInfo("Delated videos: {$compteurDelatedVideo}");
 
-        $output->writeln('<bg=cyan;fg=white>Fin du traitement</>');
-
+        $output->writeln('<bg=cyan;fg=red>Fin du traitement</>');
     }
 
-    private function _getFeedData()
+    /**
+     * Convert Date Interval into total seconds
+     *
+     * @param \DateInterval $delta
+     *
+     * @return int
+     */
+    private function toSeconds(\DateInterval $delta)
     {
-        $feed = $this->feedUrl . '?v=2&alt=jsonc';
+        $seconds = ($delta->s)
+            + ($delta->i * 60)
+            + ($delta->h * 60 * 60)
+            + ($delta->d * 60 * 60 * 24)
+            + ($delta->m * 60 * 60 * 24 * 30)
+            + ($delta->y * 60 * 60 * 24 * 365);
 
-        $json = file_get_contents($feed);
-        $data = json_decode($json);
-
-        return $data->data;
+        return (int) $seconds;
     }
 
+    /**
+     * Delete video
+     *
+     * @param \Mongobox\Bundle\JukeboxBundle\Entity\Videos $video
+     */
+    private function deleteVideo(\Mongobox\Bundle\JukeboxBundle\Entity\Videos $video)
+    {
+
+        #### Videos Group
+        $videosGroup = $this->em->getRepository('MongoboxJukeboxBundle:VideoGroup')->findBy(
+            array('video' => $video)
+        );
+
+        foreach ($videosGroup as $videoGr) {
+
+            #### Playlist
+            $videosPlaylist = $this->em->getRepository('MongoboxJukeboxBundle:Playlist')->findBy(
+                array('video_group' => $videoGr)
+            );
+
+            foreach ($videosPlaylist as $videoPlaylist) {
+                $this->em->remove($videoPlaylist);
+            }
+
+            $this->em->remove($videoGr);
+        }
+
+
+        #### Users Favoris
+        $usersFavorites = $this->em->getRepository('MongoboxBookmarkBundle:UserFavoris')->findBy(
+            array('video' => $video)
+        );
+
+        foreach ($usersFavorites as $videoFavorite) {
+            $this->em->remove($videoFavorite);
+        }
+
+        #### Video
+        $this->em->remove($video);
+
+        $this->em->flush();
+    }
 }
